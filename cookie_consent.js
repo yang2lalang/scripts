ce1f53fe1cc66626e25bf1d92847c62eca64d10e
Franklin OKOLI <franklin.okoli@get-omny.com>
2024-03-10 15:48:45 +0100

    First commit

---
+++ cookie_consent.js
@@ -0,0 +1,80 @@
+<!-- Cookie Consent by TermsFeed https://www.TermsFeed.com -->
+<script type="text/javascript" src="https://www.termsfeed.com/public/cookie-consent/4.1.0/cookie-consent.js" charset="UTF-8"></script>
+<script type="text/javascript" charset="UTF-8">
+document.addEventListener('DOMContentLoaded', function () {
+cookieconsent.run({"notice_banner_type":"interstitial","consent_type":"implied","palette":"light","language":"en","page_load_consent_levels":["strictly-necessary","functionality","tracking","targeting"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"page_refresh_confirmation_buttons":false,"website_name":"booking ternes"});
+});
+</script>
+
+<!-- initialize gtag -->
+<script type="text/plain" data-cookie-consent="strictly-necessary">
+
+// Define dataLayer and the gtag function.
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+
+// Set default consent to 'denied' as a placeholder
+// Determine actual values based on your own requirements
+gtag('consent', 'default', {
+  'ad_storage': 'denied',
+  'ad_user_data': 'denied',
+  'ad_personalization': 'denied',
+  'analytics_storage': 'denied'
+});
+</script>
+<!-- Google tag (gtag.js) -->
+<script type="text/plain" data-cookie-consent="strictly-necessary" async src="https://www.googletagmanager.com/gtag/js?id=G-ZTSG5CRJ68">
+</script>
+<script type="text/plain" data-cookie-consent="strictly-necessary">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+
+ gtag('js', new Date());
+ gtag('config', 'id=G-ZTSG5CRJ68');
+ </script> 
+<!-- end of initialize gtag-->
+
+<!-- ad_storage_granted -->
+<script type="text/plain" data-cookie-consent="targeting">
+   gtag('consent', 'update', {
+      'ad_storage': 'granted'
+    });
+</script>
+
+<!-- end of ad_storage_granted-->
+
+<!-- ad_personalization_granted -->
+<script type="text/plain" data-cookie-consent="targeting">
+   gtag('consent', 'update', {
+      'ad_personalization': 'granted'
+    });
+</script>
+<!-- end of ad_personalization_granted-->
+
+<!-- ad_user_data_granted -->
+<script type="text/plain" data-cookie-consent="targeting">
+   gtag('consent', 'update', {
+      'ad_user_data': 'granted'
+    });
+</script>
+<!-- end of ad_user_data_granted-->
+
+<!-- analytics_storage_granted -->
+<script type="text/plain" data-cookie-consent="targeting">
+   gtag('consent', 'update', {
+      'analytics_storage': 'granted'
+    });
+</script>
+<!-- end of analytics_storage_granted-->
+
+<noscript>Free cookie consent management tool by <a href="https://www.termsfeed.com/">TermsFeed</a></noscript>
+<!-- End Cookie Consent by TermsFeed https://www.TermsFeed.com -->
+
+
+
+
+
+<!-- Below is the link that users can use to open Preferences Center to change their preferences. Do not modify the ID parameter. Place it where appropriate, style it as needed. -->
+
+<a href="#" id="open_preferences_center">Update cookies preferences</a>
+
